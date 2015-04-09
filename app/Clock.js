cca9e93a478abb3efeb962c7ba13db0a2859fa01
Chris Stadler <christopher.stadler01@gmail.com>
2015-04-08 22:36:43 -0500

    Commit of rough working clock

--- app/Clock.js
+++ app/Clock.js
@@ -1,24 +1,33 @@
-import {Component, Template, bootstrap, If} from 'angular2/angular2';
+import {Component, Template, bootstrap} from 'angular2/angular2';
 
 @Component({
     selector: 'a2-clock'
 })
 @Template({
-    url: 'templates/Clock.html',
-    directives: [If]
+    url: 'templates/Clock.html'
 })
 class Clock {
-    time: Date = new Date();
-    secondHandPosition: string = 'M150, 150 L150, 2';
-    constructor() {
-        setTimeout(function() {
-            this.secondHandPosition = 'M150, 150 L150, 298';
-        }, 3000);
-    }
-    getSecondHandPosition() {
-        return this.secondHandPosition;
-    }
+    time: Date = new Date(2015, 3, 8, 12, 0, 0, 0, 0);
 
+    constructor() {
+        // this.incrementSeconds();
+    }
+    incrementSeconds() {
+        setTimeout(function() {
+            this.incrementSeconds();
+
+            this.time = new Date();
+        }.bind(this), 1000);
+    }
+    getHour(): string {
+        return 'rotate(' + (30 * (this.time.getHours() % 12) + this.time.getMinutes() / 2) + ' 50 50)';
+    }
+    getMinute(): string {
+        return 'rotate(' + (6 * this.time.getMinutes()) + ' 50 50)';
+    }
+    getSeond(): string {
+        return 'rotate(' + (6 * this.time.getSeconds()) + ' 50 50)';
+    }
 }
 
 export function main() {
