75da69c2206f9ff03c642f02858c4eedd6d4de6f
Chris Stadler <christopher.stadler01@gmail.com>
2015-04-09 23:54:42 -0500

    Refine positioning and related styling to ensure that the hands line up all of the time

--- app/Clock.js
+++ app/Clock.js
@@ -7,7 +7,7 @@ import {Component, Template, bootstrap} from 'angular2/angular2';
     url: 'templates/Clock.html'
 })
 class Clock {
-    time: Date = new Date(2015, 3, 8, 12, 0, 0, 0, 0);
+    time: Date = new Date();
     clockType: string = 'default';
 
     constructor() {
@@ -21,13 +21,13 @@ class Clock {
         }.bind(this), 1000);
     }
     getHour(): string {
-        return 'rotate(' + (30 * (this.time.getHours() % 12) + this.time.getMinutes() / 2) + ' 50 50)';
+        return 'rotate(' + (30 * (this.time.getHours() % 12) + this.time.getMinutes() / 2) + ' 49.5 49.5)';
     }
     getMinute(): string {
-        return 'rotate(' + (6 * this.time.getMinutes()) + ' 50 50)';
+        return 'rotate(' + (6 * this.time.getMinutes()) + ' 49.5 49.5)';
     }
     getSeond(): string {
-        return 'rotate(' + (6 * this.time.getSeconds()) + ' 50 50)';
+        return 'rotate(' + (6 * this.time.getSeconds()) + ' 49.5 49.5)';
     }
 }
 
